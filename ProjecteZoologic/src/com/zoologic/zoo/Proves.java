19480775b27c5d807d495f5ece9168d4e7878797
Arsenik <Arsenik@Arsenik-Portatil>
2016-02-17 18:03:45 +0100

    Creacion de todos los paquetes y clases.

---
+++ ProjecteZoologic/src/com/zoologic/zoo/Proves.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zoologic.zoo;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Arsenik
+ */
+public class Proves {
+
+    public static void Suma(){
+        int a=1;
+        int b=2;
+        System.out.println(a+b);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /**
+         * Afegir espais a una determinada especie
+         */
+        /*ArrayList<Espais> simions = new ArrayList<>();
+        HashMap<String,ArrayList> mono = new HashMap<>();
+        
+        simions.add(Espais.TANCAT);
+        simions.add(Espais.PISCINA);
+        
+        mono.put("Mono", simions);
+        
+        System.out.println(mono.get("Mono"));
+        
+        
+        
+        ArrayDeque<String> testerDos = new ArrayDeque<>();
+        testerDos.add("Necesitas una manzana");
+        testerDos.add("Tienes que meterla en el horno");
+        testerDos.add("Listo");
+        
+        System.out.println(testerDos.size());*/
+    }
+    
+}
